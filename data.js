1daa784a1bff2c77d9a98bf2d3e9bac755c0e1ea
cmmerritt <christiane.merritt@gmail.com>
2021-04-05 15:59:59 -0700

    add findById and findByName

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 
 /* eslint-disable */
 
-export default [
+export const pokeData = [
     {
        '_id':'5cef3501ef6005a77cd4fd17',
        'pokemon':'bulbasaur',
---
+++ pokedex/pokedex-model.js
@@ -0,0 +1,22 @@
+export const pokedexModel = [
+    {
+        pokemon: 'magikarp',
+        encountered: 1,
+        captured: 1,
+    },
+    {
+        pokemon: 'charizard',
+        encountered: 3,
+        captured: 1,
+    },
+    {
+        pokemon: 'squirtle',
+        encountered: 2,
+        captured: 0,
+    },
+    {
+        pokemon: 'weedle',
+        encountered: 1,
+        captured: 1,
+    }
+];
